515de958d64da4161f138a4ac3aed727cd2cf68a
SumedhaNagpal <sumedhanagpal@gmail.com>
2018-04-05 00:50:58 +0530

    added role in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404085516) do
+ActiveRecord::Schema.define(version: 20180404190153) do
 
   create_table "societies", force: :cascade do |t|
     t.string "society_name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180404085516) do
     t.string "last_name", default: "", null: false
     t.string "branch", default: "", null: false
     t.integer "year", default: 0, null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
